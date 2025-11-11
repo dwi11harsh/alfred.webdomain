923c1c8723c596c9e2da03b86ca9814360f67f4e
Prabhat Dwivedi <prabhatdwivedi411@icloud.com>
2025-11-12 00:04:58 +0530

    finished /template endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import OpenAI from "openai";
+import { BASE_PROMPT } from "./templates/basePrompt.js";
+import { basePrompt as nodeBasePrompt } from "./templates/node.js";
+import { basePrompt as reactBasePrompt } from "./templates/react.js";
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -37,27 +40,53 @@ app.post("/template", async (req, res) => {
       .status(403);
     return;
   }
+
+  if (choice == "react") {
+    res.json({
+      prompts: [
+        BASE_PROMPT,
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+      ],
+      uiPrompts: [reactBasePrompt],
+    });
+    return;
+  }
+
+  if (choice === "node") {
+    res.json({
+      prompts: [
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+      ],
+      uiPrompts: [nodeBasePrompt],
+    });
+    return;
+  }
+
+  res.status(403).json({ message: "You cant access this" });
+  return;
 });
 
-async function main() {
-  const stream = await client.chat.completions.create({
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: "write the code for todo application" },
-    ],
-    model: "gpt-4o",
-    temperature: 0,
-    max_completion_tokens: 1000,
-    stream: true,
-  });
-  console.log("\n=================================================");
-  for await (const chunk of stream) {
-    const content = chunk.choices[0]?.delta?.content || "";
-    if (content) {
-      process.stdout.write(content);
-    }
-  }
-  console.log("\n=================================================");
-}
+app.listen(3000);
 
-main();
+// async function main() {
+//   const stream = await client.chat.completions.create({
+//     messages: [
+//       { role: "system", content: "You are a helpful assistant." },
+//       { role: "user", content: "write the code for todo application" },
+//     ],
+//     model: "gpt-4o",
+//     temperature: 0,
+//     max_completion_tokens: 1000,
+//     stream: true,
+//   });
+//   console.log("\n=================================================");
+//   for await (const chunk of stream) {
+//     const content = chunk.choices[0]?.delta?.content || "";
+//     if (content) {
+//       process.stdout.write(content);
+//     }
+//   }
+//   console.log("\n=================================================");
+// }
+
+// main();
